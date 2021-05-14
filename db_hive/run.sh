80ab67cbc3446f26306fd1b5eb77388b20324ead
Edson Ramiro Lucas Filho <edson.lucas@uni-passau.de>
2021-05-14 15:53:15 +0200

    fix: runtime needs to be calculated before printed

--- db_hive/run.sh
+++ db_hive/run.sh
@@ -23,8 +23,8 @@ for trial in {1..3}; do
         begin=$(date +%s%N)
         /usr/bin/time -v hive -f db_hive/queries/${i}.sql 2> ${logdir}/${i}.err 1> ${logdir}/${i}.txt
         end=$(date +%s%N)
-        echo "$(echo "scale=2; ${runtime} / 10^9" | bc -l) (sec)"
         runtime=$(echo "scale=2; $end - $begin" | bc -l)
+        echo "$(echo "scale=2; ${runtime} / 10^9" | bc -l) (sec)"
         echo $i,$begin,$end,$runtime >> ${logdir}/${output}
     done
 done
